1e88739027628b66c97dec1ff067ba9020226bf4
unknown <harshithatu302003@gmail.com>
2024-04-24 15:30:39 +0530

    everthing is working

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -16,9 +16,10 @@ const generateAccessAndRefreshTokens=async(userId) =>
        const user= await User.findById(userId)
       const accessToken = user.generateAccessToken()
       const refreshToken=user.generateRefreshToken()
-      
+      //console.log(refreshToken);
       user.refreshToken=refreshToken
       await user.save({validateBeforeSave: false})
+    
     return {accessToken,refreshToken}
       
 
@@ -187,6 +188,7 @@ const logoutUser=asyncHandler(async(req,res) =>
 const refreshAccessToken=asyncHandler(async(req,res) =>
 {
     const incomingRefreshToken =req.cookies.refreshToken || req.body.refreshToken
+    //console.log(incomingRefreshToken);
     if(!incomingRefreshToken)
     {
         throw new ApiError(401,"unauthorizes request")
@@ -195,27 +197,30 @@ const refreshAccessToken=asyncHandler(async(req,res) =>
         const decodedToken=jwt.verify(
             incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET
     )
-       const user= User.findById(decodedToken?._id)
+    //console.log(decodedToken);
+    const user = await User.findById(decodedToken?._id); 
        if(!user)
        {
         throw new ApiError(401,"Invalid refresh token")
        }
-       if(incomingRefreshToken !== user?.refreshToken)
-       {
-        throw new ApiError(401,"Refresh token is expired or used")
-       }
+       //console.log(user);
+       if (incomingRefreshToken !== user.refreshToken) {
+        throw new ApiError(401, "Refresh token is expired or used");
+    }
+    
     
        const options=
        {
         httpOnly:true,
         secure:true
        }
-       const {accessToken,newrefreshToken}=await generateAccessAndRefreshTokens(user._id)
-       return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",newrefreshToken,options).json(
+       const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
+       //console.log(refreshToken);
+       return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",refreshToken,options).json(
         new ApiResponse(
             200,
             {
-                accessToken,refreshToken:newrefreshToken},"access Token Refreshed"
+                accessToken,refreshToken:refreshToken},"access Token Refreshed"
             
         )
        )
